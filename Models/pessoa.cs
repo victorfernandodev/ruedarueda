cc6a425901ad4f96c0827ecb2b7e6a2874e17dd0
Victor Fernando Dev <53957221+victorfernandodev@users.noreply.github.com>
2020-08-26 10:47:03 -0300

    Add files via upload

---
+++ Models/pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string DtNasc { get; set; }
+        public float Salario { get; set; }
+    }
+}
